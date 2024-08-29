8e78ee15d1cdcca6516d821601f9173e65a885d4
kunal-dhole <kdhole59@gmail.com>
2024-08-30 00:58:19 +0530

    first commit

---
+++ src/com/jdbc/Select.java
@@ -0,0 +1,14 @@
+package com.jdbc;
+
+
+public class Select {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("step2");
+         
+	 
+	}
+
+}
